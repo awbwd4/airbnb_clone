bdf6240569cb306afcddcf75fd3baa31c83bd4bd
awbwd4 <awbwd4@gmail.com>
2021-10-01 01:23:04 +0900

    20210930

--- rooms/models.py
+++ rooms/models.py
@@ -29,28 +29,45 @@ class RoomType(AbstractItem):
 
     """RoomType Object Definition"""
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
 
 
 class Amenity(AbstractItem):
 
     """Amenity Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """Facilities Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """HouseRule Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampModel):
+
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    rooms = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampModel):
@@ -72,9 +89,14 @@ class Room(core_models.TimeStampModel):
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+    # host = models.ForeignKey("User", on_delete=models.CASCADE)
+    # room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    # amenities = models.ManyToManyField("Amenity", blank=True)
+    # facilities = models.ManyToManyField("Facility", blank=True)
+    # house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
