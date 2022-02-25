1966702a8bf10e894ac79142085e909e9d6007ed
awbwd4 <awbwd4@gmail.com>
2022-02-25 19:29:57 +0900

    22.02.25 pagination -1

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from datetime import datetime
 from pickle import TRUE
 from django.shortcuts import render
@@ -5,9 +6,22 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    print(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    ## page : 2 -> 10~20까지 보여줘야 함 -> limit = 10*2 = 20 // offset = 20-10 = 10
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": all_rooms},
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
     )
