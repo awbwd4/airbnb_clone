793bac000c696a7a225af6976ce71ba1b4c300cd
awbwd4 <awbwd4@gmail.com>
2022-03-31 19:11:40 +0900

    22.03.31 Sign Up with Form

--- users/views.py
+++ users/views.py
@@ -11,6 +11,12 @@ class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
+    # success_url = reverse("core:home")
+    # reverse : "core:home"으로 되돌려 보내주도록 되어 있으나
+    # 이 클래스(form)을 가져올때는 url은 아직 호출이 되지 않은 상태임.
+    # 즉, config.url이 아직 어떤 패턴도 갖고 있지 않음.
+    # reverse_lazy : reverse랑 같은 기능이긴 한데, 자동으로 url을 호출하진 않음.
+    # view가 필요할때만 호출
 
     def form_valid(self, form):
         print("cleand_data [%s]" % form.cleaned_data)
@@ -33,3 +39,26 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Jaeuk",
+        "last_name": "Ko",
+        "email": "jaeuk@naver.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+
+        # 가입이 성공하면 바로 로그인
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
