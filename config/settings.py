7a544b8785e4fbf7ab4d34bd532f06ddc7e7e2d9
awbwd4 <awbwd4@gmail.com>
2022-04-07 21:18:57 +0900

    22.04.07 tailwind css -1

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/"  # static url, 이건 뭘로 바껴도 됨.
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # static 폴더
 
 
 AUTH_USER_MODEL = "users.User"
